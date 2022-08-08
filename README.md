054294e254da069b1420b9c579db719220a0f64b
Bernhard Kircher <bernhardkircher@gmail.com>
2022-08-08 19:59:57 +0200

    Initial commit - add install script and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+About
+==========
+
+This is a powershell script that I use to setup a new machine. It needs to be run as an Administrator - so be carefull what is in the script.
+It uses chocolatey (see https://docs.chocolatey.org/) to install applications and update them.
+
+You might need to open powershell as an administrator and make sure to be able to execute the script, e.g:
+```` > Set-ExecutionPolicy Bypass -Scope  CurrentUser
+and then execute:
+```` > .\install.ps1
+
---
+++ install.ps1
@@ -0,0 +1,24 @@
+$appsToInstall = 'chocolateygui', 'bitwarden', 'microsoft-windows-terminal', 'sysinternals', 'vlc', 'powertoys', 'paint.net', '7zip.install', 'dotnetcore-sdk', 'git.install', `
+			'tortoisegit', 'notepadplusplus.install', 'visualstudiocode', 'postman', 'googlechrome', 'firefox', 'jetbrainstoolbox', `
+			'jetbrains-rider', 'datagrip', 'sql-server-management-studio', 'wsl2', 'wsl-ubuntu-2204'
+
+
+$testchoco = powershell choco -v
+# !$? means: last operation was not succesfull
+if(!$?){
+    Write-Output "Seems Chocolatey is not installed, installing now"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+else{
+    Write-Output "Chocolatey Version $testchoco is already installed"
+}
+
+foreach($app in $appsToInstall) {
+	Write-Host "Installing Application " $app
+	choco install $app -y
+}
+
+# to install windows features:
+# Dism /online /Get-Features
+# choco list --source windowsFeatures
+# choco install [Feature Name] /y /source windowsfeatures
