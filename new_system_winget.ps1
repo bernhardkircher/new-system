f08b06138ac5cff7c34386e5bf79a2f5fa6e30d7
bernhardkircher <bernhardkircher@gmail.com>
2025-01-07 20:14:59 +0100

    Create new_system_winget.ps1

---
+++ new_system_winget.ps1
@@ -0,0 +1,31 @@
+
+winget install -e --id OpenJS.NodeJS.LTS
+winget install -e --id CoreyButler.NVMforWindows
+winget install -e --id Notepad++.Notepad++
+winget install -e --id Microsoft.VisualStudio.2022.Community
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Ghisler.TotalCommander
+winget install -e --id Microsoft.SQLServer.2022.Developer
+winget install -e --id Microsoft.SQLServerManagementStudio
+winget install -e --id LINQPad.LINQPad.7
+
+winget install -e --id Microsoft.PowerToys
+winget install -e --id Google.Chrome
+winget install -e --id Mozilla.Firefox
+winget install -e --id JGraph.Draw
+
+winget install -e --id Postman.Postman
+
+winget install -e --id mRemoteNG.mRemoteNG
+winget install -e --id Bitwarden.Bitwarden
+winget install -e --id Greenshot.Greenshot
+winget install -e --id 7zip.7zip
+winget install -e --id Git.Git 
+winget install -e --id TortoiseGit.TortoiseGit
+winget install -e --id dotPDNLLC.paintdotnet
+winget install -e --id tailscale.tailscale 
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id JanDeDobbeleer.OhMyPosh
+winget install -e --id Microsoft.DotNet.Framework.DeveloperPack_4
+winget install -e --id Microsoft.IronPython.3
+winget install -e --id Microsoft.AzureDataStudio
